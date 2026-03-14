e5153c6656b5b2598607718cd688bd781d47e28f
Shikhar Srivastava <shikhavv.05@gmail.com>
2026-03-15 00:09:36 +0530

    Initial project commit

--- Build_og_Git/langchain_agent/agent.py
+++ Build_og_Git/langchain_agent/agent.py
@@ -4,7 +4,7 @@ from langchain_agent.tools import get_aqi_forecast
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key="AIzaSyChVVloFEU1iog7nMd8oS3UjhLytH2KoC4",
+    google_api_key="",
     temperature=0.3
 )
 
--- Build_og_Git/langchain_agent/llm_config.py
+++ Build_og_Git/langchain_agent/llm_config.py
@@ -3,6 +3,6 @@ import os
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key="AIzaSyChVVloFEU1iog7nMd8oS3UjhLytH2KoC4",
+    google_api_key="",
     temperature=0.3
 )
